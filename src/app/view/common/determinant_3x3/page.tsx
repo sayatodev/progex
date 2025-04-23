413b3d72b89c1ba40395adcaba75b012a3936f46
Sayako Hazuki <amf.kano@gmail.com>
2025-04-23 15:16:13 +0800

    Initial Template

---
+++ src/app/view/common/determinant_3x3/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+      <>
+        <main className="flex min-h-screen flex-col items-center gap-3 p-24">
+          <h1 className="text-4xl font-bold">Determinant of 3x3 Matrix</h1>
+          <pre>?→A:?→B:?→C:?→D:?→X:?→Y:?→M:BYM-MXC:?→M:Ans+DMC-MYA:?→M:Ans+AXM-MDB</pre>
+        </main>
+      </>
+    );
+  }
+  
